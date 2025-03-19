60c9366a48705ac8550d3fcfa737a0681af3020c
Imtiaz Ahmed <imtiazali78652@gmail.com>
2025-03-20 02:23:29 +0500

    last

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(cors());
   app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
   });
+  app.get("/", (req, res) => res.send("Hello World!"));
 })();
 
 app.use("/api", routes);
